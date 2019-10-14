6a7178999af56f5933772c6bbcc6fb43c29abc2f
Terrazzoni Jean-Baptiste <jterrazz@protonmail.com>
2019-10-14 20:45:51 +0200

    Program header dump ok in 64bits

--- program/src/elf_program_header.c
+++ program/src/elf_program_header.c
@@ -15,40 +15,49 @@ int dump_program_header_64(void *bin_start, size_t bin_len)
     Elf64_Phdr *phdr = secure_read(bin_start, bin_len, offset, header->e_phnum * header->e_phentsize);
     ft_printf("Program Headers:\n");
     ft_printf("Type    Offset       VirtualAddress     PhysAddr\n");
+
+    Elf64_Phdr *last_load_phdr = NULL;
     for (u16 i = 0; i < header->e_phnum ; i++) {
         switch (phdr->p_type) {
             case PT_NULL:
-                ft_printf("ULL\n");
+                ft_printf("%-20s", "ULL");
                 break;
             case PT_LOAD:
-                ft_printf("LOAD\n");
+                ft_printf("%-20s", "LOAD");
+                last_load_phdr = phdr;
                 break;
             case PT_DYNAMIC:
-                ft_printf("DYNAMIC\n");
+                ft_printf("%-20s", "DYNAMIC");
                 break;
             case PT_INTERP:
-                ft_printf("INTERP\n");
+                ft_printf("%-20s", "INTERP");
                 break;
             case PT_NOTE:
-                ft_printf("NOTE\n");
+                ft_printf("%-20s", "NOTE");
                 break;
             case PT_SHLIB:
-                ft_printf("SHLIB\n");
+                ft_printf("%-20s", "SHLIB");
                 break;
             case PT_PHDR:
-                ft_printf("PHDR\n");
+                ft_printf("%-20s", "PHDR");
                 break;
             case PT_GNU_RELRO:
-                ft_printf("GNU_RELRO\n");
+                ft_printf("%-20s", "GNU_RELRO");
+                break;
+            case PT_GNU_EH_FRAME:
+                ft_printf("%-20s", "GNU_EH_FRAME");
                 break;
             case PT_LOPROC:
             case PT_HIPROC:
-                ft_printf("LOPROC, HIPROC\n");
+                ft_printf("%-20s", "LOPROC, HIPROC");
                 break;
             case PT_GNU_STACK:
-                ft_printf("GNU_STACK\n");
+                ft_printf("%-20s", "GNU_STACK");
                 break;
         }
+        ft_printf("%10p %10p %10p\n", phdr->p_offset, phdr->p_vaddr, phdr->p_paddr);
         phdr++;
     }
+
+    ft_printf("Last LOAD header is at offset: %p with size %lld", last_load_phdr->p_offset, last_load_phdr->p_memsz);
 }
